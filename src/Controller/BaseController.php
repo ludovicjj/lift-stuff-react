d025b26c201302a7dfda91618e101eac12134890
ludovicjj <jahanlud@gmail.com>
2022-09-08 13:00:53 +0200

    Create entry point listing reps

---
+++ src/Controller/BaseController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Controller;
+
+use App\Api\RepLogModel;
+use App\Entity\RepLog;
+use App\Repository\RepLogRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Serializer\SerializerInterface;
+
+class BaseController extends AbstractController
+{
+    public function __construct(
+        private RepLogRepository $repLogRepository,
+        private SerializerInterface $serializer
+    )
+    {
+    }
+
+    private function buildRepLogModel(RepLog $repLog): RepLogModel
+    {
+        $model = new RepLogModel();
+        $model->id = $repLog->getId();
+        $model->reps = $repLog->getReps();
+        $model->item = $repLog->getItem();
+        $model->totalWeightLifted = $repLog->getTotalWeightLifted();
+        $model->addLink('self', 'todo url to delete self');
+
+        return $model;
+    }
+
+    protected function createApiResponse($data, $statusCode = 200): Response
+    {
+        $json = $this->serializer->serialize($data, 'json');
+        return new JsonResponse($json, $statusCode, [], true);
+    }
+
+    /**
+     * @return RepLogModel[]
+     */
+    protected function findAllRepLogModels(): array
+    {
+        $repLogs = $this->repLogRepository->findBy(['user' => $this->getUser()]);
+        $models = [];
+
+        foreach ($repLogs as $repLog) {
+            $models[] = $this->buildRepLogModel($repLog);
+        }
+
+        return $models;
+    }
+}
--- src/Controller/LiftController.php
+++ src/Controller/LiftController.php
@@ -2,7 +2,19 @@
 
 namespace App\Controller;
 
-class LiftController
-{
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
+#[Route("/api", name: "rep_log_")]
+class LiftController extends BaseController
+{
+    #[Route("/reps", name: "list", methods: ['GET'])]
+    public function listRep(): Response
+    {
+        $models = $this->findAllRepLogModels();
+
+        return $this->createApiResponse([
+            'items' => $models
+        ]);
+    }
 }
