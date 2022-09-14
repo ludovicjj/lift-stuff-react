8e94e516cad55c968a9fa49bfb54b673fb767cf9
ludovicjj <jahanlud@gmail.com>
2022-09-14 17:11:27 +0200

    Subscriber Kernel_exception

---
+++ src/Subscriber/ExceptionSubscriber.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Subscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+
+class ExceptionSubscriber implements EventSubscriberInterface
+{
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            KernelEvents::EXCEPTION => ['onKernelException', 2]
+        ];
+    }
+
+    public function onKernelException(ExceptionEvent $event): void
+    {
+        $request = $event->getRequest();
+        if ($request->isXmlHttpRequest()) {
+            $this->sendJsonResponseException($event);
+        }
+    }
+
+    public function sendJsonResponseException(ExceptionEvent $event): void
+    {
+        $exception = $event->getThrowable();
+        $statusCode = $exception->getCode() ?: 400;
+        $data = [
+            'message'   => $exception->getMessage(),
+            'code'      => $statusCode
+        ];
+//        if ($exception instanceof ValidationException) {
+//            $data['errors'] = $exception->getErrors();
+//        }
+
+        $event->setResponse(new JsonResponse($data, $statusCode));
+    }
+}
