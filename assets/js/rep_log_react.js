a403925407ea5498c500acac468cc27bcc170f75
ludovicjj <jahanlud@gmail.com>
2022-09-26 14:12:53 +0200

    Define RepLogApp props using object rest spread on window.REP_LOG_APP_PROPS

--- assets/js/rep_log_react.js
+++ assets/js/rep_log_react.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { createRoot } from 'react-dom/client';
 import RepLogApp from "./RepLog/RepLogApp";
+import '../styles/rep_log.scss';
+import Accordion from "./Components/Accordion";
 
-const shouldShowHeart = true;
-const itemOptions = [
-    {id: 'cat', text: 'Cat'},
-    {id: 'fat_cat', text: 'Big Fat Cat'},
-    {id: 'laptop', text: 'My Laptop'},
-    {id: 'coffee_cup', text: 'Coffee Cup'},
-    {id: 'invalid_item', text: 'Bad item'}
-]
+const acc = document.querySelector(".accordion-container");
+new Accordion(acc);
+
+const shouldShowHeart = false;
 const root = createRoot(document.getElementById('lift-stuff-app'))
-root.render(<RepLogApp withHeart={shouldShowHeart} itemOptions={itemOptions}/>)
+// window.REP_LOG_APP_PROPS.withHeart override shouldShowHeart
+// Last props override first props
+root.render(<RepLogApp withHeart={shouldShowHeart} {...window.REP_LOG_APP_PROPS}/>)
